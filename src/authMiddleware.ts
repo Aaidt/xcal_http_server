469b7aee2cf68bc52bf773b588892faee59947a7
Aadit <aadits2004@gmail.com>
2025-09-02 21:07:46 +0530

    middleware updated

--- src/authMiddleware.ts
+++ src/authMiddleware.ts
@@ -6,21 +6,23 @@ dotenv.config();
 const jwt_secret = process.env.JWT_SECRET as string;
 
 declare global {
-    namespace Express {
-        interface Request {
-            userId: string;
-        }
+  namespace Express {
+    interface Request {
+      userId: string;
     }
+  }
 }
 
-export default function authMiddleware (req: Request, res: Response, next: NextFunction) {
-    const header = req.headers["authorization"] || "";
-    const verified = jwt.verify(header, jwt_secret) as jwt.JwtPayload;
+export default function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const header = req.headers["authorization"] || "";
+  const authHeader = header.split(" ")[1]
+  if (!authHeader) return res.json({ message: "No authentication header provided." })
+  const verified = jwt.verify(authHeader, jwt_secret) as jwt.JwtPayload;
 
-    if(verified){
-        req.userId = verified.userId;
-        next();
-    }else{
-        throw new Error("Not authorized.");
-    }
+  if (verified) {
+    req.userId = verified.userId;
+    next();
+  } else {
+    throw new Error("Not authorized.");
+  }
 }

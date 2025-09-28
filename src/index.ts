e38f7d867ece1792cf64d887b7b5341315caebaf
Aadit <aadits2004@gmail.com>
2025-09-28 20:43:25 +0530

    cluster multithreading for server

--- src/index.ts
+++ src/index.ts
@@ -4,52 +4,78 @@ import cors from "cors"
 import roomRouter from "./routes/roomRouter"
 import { requireAuth } from "@clerk/express";
 import { prismaClient } from "./prisma/export";
-
-const app = express();
-
-app.use(express.json())
-app.use(cors({
-   credentials: true,
-   origin: [
-      "http://localhost:3001",
-      "https://xcal-fe.vercel.app",
-      "https://xcal.codexbuild.website"
-   ]
-}));
+import cluster from "cluster"
+import os from "os";
 
 
-// routes
-app.get("/api/me", requireAuth(), async function(req: Request, res: Response) {
-   const userId = req.auth?.userId
-   if (!userId) {
-      res.status(404).json({ message: "No userId recieved." })
-      return
+if (cluster.isPrimary) {
+   const total_cpus = os.cpus().length;
+   console.log(`Primary process: ${process.pid} has started...`);
+
+   for (let i = 0; i < total_cpus; i++) {
+      cluster.fork();
    }
 
-   try {
-      const userAlreadyExists = await prismaClient.user.findUnique({
-         where: { id: userId }
-      });
+   cluster.on("exit", (worker) => {
+      console.log(`Worker process ${worker.process.pid} died. Creating a new one...`);
+      cluster.fork();
+   })
+} else {
+   console.log(`Worker process: ${process.pid} has started...`);
 
-      if (!userAlreadyExists) {
-         await prismaClient.user.create({
-            data: { id: userId }
-         })
+   const app = express();
 
-         res.status(200).json({ message: "User is now in the db." })
-      } else {
-         res.status(200).json({ message: "User is already in the db" })
+   app.use(express.json())
+   app.use(cors({
+      credentials: true,
+      origin: [
+         "http://localhost:3001",
+         "https://xcal-fe.vercel.app",
+         "https://xcal.codexbuild.website"
+      ]
+   }));
+
+
+   // routes
+   app.get("/api/me", requireAuth(), async function (req: Request, res: Response) {
+      const userId = req.auth?.userId
+      if (!userId) {
+         res.status(404).json({ message: "No userId recieved." })
+         return
       }
 
+      try {
+         const userAlreadyExists = await prismaClient.user.findUnique({
+            where: { id: userId }
+         });
 
-   } catch (err) {
-      console.log("Server error: " + err);
-      res.status(500).json({ message: "Server error " + err });
-   }
-})
-app.use("/api/room", requireAuth(), roomRouter)
-app.get("/health", (req: Request, res: Response) => {
-   res.json({ message: "server is healthy" })
-})
+         if (!userAlreadyExists) {
+            await prismaClient.user.create({
+               data: { id: userId }
+            })
 
-app.listen(3002, () => { console.log("Server is listening on port 3002") });
+            res.status(200).json({ message: "User is now in the db." })
+         } else {
+            res.status(200).json({ message: "User is already in the db" })
+         }
+
+
+      } catch (err) {
+         console.log("Server error: ", err);
+         res.status(500).json({ message: "Server error " + err });
+      }
+   })
+   app.use("/api/room", requireAuth(), roomRouter)
+   app.get("/health", (req: Request, res: Response) => {
+      res.json({ message: "server is healthy" })
+   })
+
+   const server = app.listen(3002, () => { console.log("Server is listening on port 3002") });
+
+
+   process.on("SIGTERM", async () => {
+      console.log(`worker thread ${process.pid} is shutting down...`)
+      await prismaClient.$disconnect();
+      server.close(() => process.exit(0));
+   })
+}
